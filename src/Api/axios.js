ecce570315d125bfdabc9b957e7b276159a146dc
12-selam <kidaneselam778@yahoo.co.uk>
2025-06-02 17:10:57 +0100

    updte

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/project-d57e5/us-central1/api"
+    baseURL:"https://amazon2-backend.onrender.com"
+    // baseURL:"http://127.0.0.1:5001/"
+    // baseURL:"http://127.0.0.1:5001/project-d57e5/us-central1/api"
+
+    //deploy version of firebase function
+    // baseURL:"http://127.0.0.1:5001/project-d57e5/us-central1/api"
 
 
 
